0adf3c1b0dea8745e1f7059af0ac230774684cd3
Michał Brzozowski <62841881+MichalBrzozowski91@users.noreply.github.com>
2020-09-03 22:51:04 +0200

    Create 0145_Binary_Tree_Postorder_Traversal_Iterative.py

---
+++ leetcode/medium/0145_Binary_Tree_Postorder_Traversal_Iterative.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        # Iterative solution
+        if not root:
+            return []
+        result = []
+        nodes = [[root,1]]
+        while nodes:
+            node = nodes[-1]
+            tree = node[0]
+            flag = node[1]
+            if flag == 0:
+                result.append(tree.val)
+                nodes.pop()
+            else:
+                nodes[-1][1] = 0 # Node was open
+                if tree.right:
+                    nodes.append([tree.right,1])
+                if tree.left:
+                    nodes.append([tree.left,1])
+        return result
