5eabde26d596802bed217e8aeb8784f3e43e13d5
Michał Brzozowski <62841881+MichalBrzozowski91@users.noreply.github.com>
2020-09-14 23:54:26 +0200

    Create 0199_Binary_Tree_Right_Side_View.py

---
+++ leetcode/medium/0199_Binary_Tree_Right_Side_View.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        result = []
+        # DFS
+        nodes = [root]
+        new_nodes = []
+        while nodes:
+            result.append(nodes[-1].val)
+            for node in nodes:
+                if node.left:
+                    new_nodes.append(node.left)
+                if node.right:
+                    new_nodes.append(node.right)
+            nodes = new_nodes
+            new_nodes = []
+        return result
